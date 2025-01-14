d59f32e5ffc1c4706db839b1f75a7b7828bc9df7
vishalkumar4491 <vishalkumar.05423@gmail.com>
2025-01-14 14:17:25 +0530

    Fixing UI

--- src/main/java/com/portifolio/wealthinker/auth/controllers/AuthController.java
+++ src/main/java/com/portifolio/wealthinker/auth/controllers/AuthController.java
@@ -29,8 +29,7 @@ public class AuthController {
 
     // Render the login page with potential error messages
     @RequestMapping("/login")
-    public String loginPage(){
-        
+    public String loginPage(){ 
         return "auth/login"; // the login page template
     }
 
@@ -38,7 +37,7 @@ public class AuthController {
     @RequestMapping("/register")
     public String signupPage(Model model) {
         UserFormDTO userFormDTO = new UserFormDTO();
-        // userForm.setName("VK");
+        // userFormDTO.setName("VK");
         model.addAttribute("userFormDTO", userFormDTO);
         return "auth/register";
     }
@@ -63,29 +62,25 @@ public class AuthController {
 
 
         User user = new User();
-            user.setName(userForm.getName());
-            user.setEmail(userForm.getEmail());
-            user.setPassword(userForm.getPassword());
-            user.setAbout(userForm.getAbout());
-            user.setUsername(userForm.getUsername());
-            user.setPhoneNumber(userForm.getPhoneNumber());
-            // user.setProfilePic("https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3Dpic&psig=AOvVaw3DqSCABBYScvwhLtkuYJND&ust=1729407144470000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNDth-btmYkDFQAAAAAdAAAAABAO");
+        user.setName(userForm.getName());
+        user.setEmail(userForm.getEmail());
+        user.setPassword(userForm.getPassword());
+        user.setAbout(userForm.getAbout());
+         user.setUsername(userForm.getUsername());
+        user.setPhoneNumber(userForm.getPhoneNumber());
+        // user.setProfilePic("https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3Dpic&psig=AOvVaw3DqSCABBYScvwhLtkuYJND&ust=1729407144470000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNDth-btmYkDFQAAAAAdAAAAABAO");
     
-            user.setEnabled(true);
+        user.setEnabled(true);
     
-            User savedUser = userService.saveUser(user);
+        User savedUser = userService.saveUser(user);
+
+        // message = "Registration Successfull"
     
-            System.out.println(savedUser);
+        Message message = Message.builder().content("Registration Successful").type(MessageType.green).build();
     
-            // message = "Registration Successfull"
+        session.setAttribute("message", message);
     
-            Message message = Message.builder().content("Registration Successful").type(MessageType.green).build();
-    
-            System.out.println("User Saved");
-    
-            session.setAttribute("message", message);
-    
-            // redirect
-            return "redirect:/register";
+        // redirect
+        return "redirect:/register";
     }
 }

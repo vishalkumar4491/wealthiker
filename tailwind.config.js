c5a5ba98321e9bfaeef7946c25fa577b7afb1256
vishal Kumar <vishalkumar.05423@gmail.com>
2025-02-17 05:52:03 +0530

    fixing home ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/main/resources/**/*.{html,js}", "./node_modules/flowbite/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: {
+          background: "#edf1f2",
+          primary: "#121212",
+          secondary: "#383e42",
+          accent: "#43e5c5",
+          text: "#040316",
+        },
+        dark: {
+          background: "#0d1112",
+          primary: "#ededed",
+          secondary: "#bdc3c7",
+          accent: "#1abc9c",
+          text: "#eae9fc",
+        },
+      },
+    },
   },
   plugins: [require('flowbite/plugin')],
   darkMode: "selector",

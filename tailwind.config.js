f28203534917d00cbed2a050d613fcaddbd1f4df
vishalkumar4491 <vishalkumar.05423@gmail.com>
2025-02-19 20:48:48 +0530

    Improvng main page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,25 @@ module.exports = {
     extend: {
       colors: {
         light: {
-          background: "#edf1f2",
+          background: "#f1f1f1",
+          background_secondary: "#ececec",
           primary: "#121212",
-          secondary: "#383e42",
+          primary_hover: "#040316",
+          secondary: "#51575c",
+          secondary_hover: "#41454A",
           accent: "#43e5c5",
+          accent_hover: "#43f1c5",
           text: "#040316",
         },
         dark: {
           background: "#0d1112",
+          background_secondary: "#232323",
           primary: "#ededed",
+          primary_hover: "#EAE9FC",
           secondary: "#bdc3c7",
+          secondary_hover: "#A8ACB2",
           accent: "#1abc9c",
+          accent_hover: "#13936E",
           text: "#eae9fc",
         },
       },
